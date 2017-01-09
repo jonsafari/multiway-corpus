d09e595540383b2769c604c8976b6a7015a3818c
Jon Dehdari <jon@dehdari.org>
2017-01-09 23:42:08 +0100

    tatoeba-intersect.py++

--- tatoeba-intersect.py
+++ tatoeba-intersect.py
@@ -5,23 +5,39 @@ import os
 
 code_freq_filename = os.path.join('data', 'lang_codes_iso-639-3_freq.tsv')
 code_filename = os.path.join('data', 'lang_codes_iso-639-3.tsv')
+
 codes = {}
 codes_rev = {}
+links = {}
+sents = {}
 
 def main():
     import sys
 
     langs = sys.argv[1:]
-    print(langs)
+    lang_set = set(langs) # for random access later
 
+    # ISO 639-3 frequencies, based on number of entries in Tatoeba dataset
     with open(code_freq_filename) as code_freq_file:
         code_freq = code_freq_file.read().split()
 
+    # Bijective mapping between ISO 639-3 language codes and their (macro-)language name.  Eg. eng:'English'
     with open(code_filename) as code_file:
         for line in code_file:
             code, lang = line.rstrip().split('\t')
             codes[code] = lang
             codes_rev[lang] = code
+
+    # Relations between translation sentences
+    with open('links.csv') as link_file:
+        ...
+
+    # Translation sentences
+    with open('sentences.csv') as sentences_file:
+        for line in sentences_file:
+            id, code, sent = line.split('\t')
+            if code in lang_set:
+                ...
 
 
 
