6cc808b9fc8cc4b555f431c0694d6b2d68aed04c
Jon Dehdari <jon@dehdari.org>
2017-01-10 22:09:41 +0100

    tatoeba-intersect.py: normalize langs from cmd-line; find smallest lang

--- tatoeba-intersect.py
+++ tatoeba-intersect.py
@@ -12,16 +12,12 @@ codes = {}
 codes_rev = {}
 links = {}
 sents = {}
+lang_set = set()
 
 def main():
     import sys
 
     langs = sys.argv[1:]
-    lang_set = set(langs) # for random access later
-
-    # ISO 639-3 frequencies, based on number of entries in Tatoeba dataset
-    with open(code_freq_filename) as code_freq_file:
-        code_freq = code_freq_file.read().split()
 
     # Bijective mapping between ISO 639-3 language codes and their (macro-)language name.  Eg. eng:'English'
     with open(code_filename) as code_file:
@@ -29,6 +25,29 @@ def main():
             code, lang = line.rstrip().split('\t')
             codes[code] = lang
             codes_rev[lang] = code
+
+    # Normalize user-supplied langage names/codes to codes (eg. eng)
+    for lang in langs:
+        if lang in codes: # code is in ISO standard
+            lang_set.add(lang)
+        elif lang in codes_rev:
+            lang_set.add(codes_rev[lang])
+        else:
+            print('"%s" is neither an ISO 639-3 code nor ISO 639-3 (macro-)language name' % lang, file=sys.stderr)
+            sys.exit()
+
+    # Find language with fewest entries in Tatoeba dataset.
+    # This was already done to get code_freq_filename, using the following command:
+    # cut -f 2 sentences.csv | sort | uniq -c | sort -rn | tr -s ' ' |
+    #   cut -d ' ' -f 3 > data/lang_codes_iso-639-3_freq.tsv
+    with open(code_freq_filename) as code_freq_file:
+        code_freq = code_freq_file.read().split()
+        least_freq_code = ''
+        for code in code_freq:
+            if code in lang_set:
+                least_freq_code = code
+        print("Smallest Language:", least_freq_code, file=sys.stderr)
+
 
     # Relations between translation sentences
     with open('links.csv') as link_file:
